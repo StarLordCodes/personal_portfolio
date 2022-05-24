71684fa648b7fb5a57246ca95015ec606a518b19
StarLordCodes <amammoottil@outlook.com>
2022-05-24 20:46:02 +0530

    Changed code in blogs/views to call the Blog.objects. Here instead of calling all the objects, we use order by to order the blogs by date and [:5] to call only 5 objects. Then added code in all_blogs.html to view objects. Then option to add static files like images,pdfs etc  was learnt. The main settings file has a static url 'static/' already set. Since home page is in portfolio app, we create a static folder in it, then a portfolio in the static folder then our required static files. To load the files on homepage.html use {% load static %}  and then call with {% static 'static/portfolio/filename' %} This is different from media root and url as the media there is uploaded by user

--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
 
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    # to show all the objects use Blog.objects.all() here we are ordering the objects by date and showing only last 5
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {"blogs": blogs})
