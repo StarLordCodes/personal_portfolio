83f6ff84dcb6b6a40aeac8515fac53e04a89de57
StarLordCodes <amammoottil@outlook.com>
2022-05-27 10:56:17 +0530

    Added a new page blog_details and then changed the views in blog to accomodate it. Changed navbar a bit more to change. changed the blog/urls.py to include blog_details. also added an app name to blog/views.py in order to avoid confusion with other apps.

--- personal_portfolio-project/blog/urls.py
+++ personal_portfolio-project/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.all_blogs, name="all_blogs"),
+    path("<int:blog_id>/", views.blog_details, name="blog_details"),
 ]
 
--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
+
+app_name = 'blog'
 
 
 def all_blogs(request):
     # to show all the objects use Blog.objects.all() here we are ordering the objects by date and showing only last 5
     blogs = Blog.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {"blogs": blogs})
+
+
+def blog_details(request, blog_id):
+    blog_object = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/blog_details.html', {"blog_object": blog_object})
